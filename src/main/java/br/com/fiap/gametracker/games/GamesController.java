54a16d3ea6902e1a68b3e08c2125d5f29778af50
Henrique Incerpi <101296567+Incerpi@users.noreply.github.com>
2023-10-02 15:04:46 -0300

    Criação do Projeto GameTracker

---
+++ src/main/java/br/com/fiap/gametracker/games/GamesController.java
@@ -0,0 +1,34 @@
+package br.com.fiap.gametracker.games;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/games")
+public class GamesController {
+
+    @Autowired
+    GamesService service;
+
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("game", service.findAll());
+        return "games/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirect){
+        if (service.delete(id)){
+            redirect.addFlashAttribute("success", "Tarefa apagada com sucesso");
+        }else{
+            redirect.addFlashAttribute("error", "Tarefa não encontrada");
+        }
+        return "redirect:/games";
+    }
+    
+}
---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO games (title, review, stars) VALUES ('God of War 2018', 'Jogo muito bom! Adorei!', 10);
+INSERT INTO games (title, review, stars) VALUES ('God of War: Ragnarok', 'Decaiu em qualidade!', 7);
+INSERT INTO games (title, review, stars) VALUES ('Hades', 'Um dos melhores jogos!', 9);
+INSERT INTO games (title, review, stars) VALUES ('Dead Cells', 'Muito bom!', 10);
