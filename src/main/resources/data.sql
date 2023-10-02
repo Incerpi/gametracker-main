88c424296954f4761404ac3ee40142e786cd4028
Incerpi <incerpih@.com>
2023-10-02 15:30:11 -0300

    alteradas as cores

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ INSERT INTO games (title, review, stars) VALUES ('God of War 2018', 'Jogo muito 
 INSERT INTO games (title, review, stars) VALUES ('God of War: Ragnarok', 'Decaiu em qualidade!', 7);
 INSERT INTO games (title, review, stars) VALUES ('Hades', 'Um dos melhores jogos!', 9);
 INSERT INTO games (title, review, stars) VALUES ('Dead Cells', 'Muito bom!', 10);
+INSERT INTO games (title, review, stars) VALUES ('Castle Crashers', 'Me diverti muito!', 7);
+INSERT INTO games (title, review, stars) VALUES ('Death Stranding', 'Muito ruim!', 4);
+INSERT INTO games (title, review, stars) VALUES ('Dragon Ball Z: Kakarot', 'Um jogo lindo!', 6);
+
